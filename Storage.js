6d5b21a46de3e75c1f406c39341717699d0b4320
nora <noraashqar244@gmail.com>
2025-09-06 16:49:44 +0300

    add loadAll method and edit on saveAll in Storage class

--- Storage.js
+++ Storage.js
@@ -4,9 +4,14 @@
     }
     saveAll(answers){
         const stored=localStorage.getItem(this.key);
-        const parsed=stored&&JSON.parse(stored);
+        const parsed=(stored&&JSON.parse(stored))??{};
         const updated={...parsed,...answers};
         localStorage.setItem(this.key,JSON.stringify(updated));
+    }
+    loadAll(){
+        const stored=localStorage.getItem(this.key);
+        const parsed= JSON.parse(stored)??{}
+        return parsed;
     }
     clearAll(){
         localStorage.removeItem(this.key);
