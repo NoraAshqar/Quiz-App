3ae32c2508b5759a34bfad00af0c41a249633973
nora <noraashqar244@gmail.com>
2025-09-06 16:05:15 +0300

    add quiz questions (data) in Questions.js file

---
+++ Questions.js
@@ -0,0 +1,41 @@
+import Question from './Question.js';
+
+export const questions = [
+  new Question(
+    1, 
+    "How many arguments can a JavaScript function accept?", 
+    ["Two", "None", "Only one", "Unlimited"], 
+    "Unlimited"
+),
+  new Question(2,
+     "What is 'Abstraction' in the context of OOP?", 
+     [
+    "The ability to inherit properties from multiple classes.",
+    "Converting an object into a JSON string.",
+    "The process of creating detailed, complex classes.",
+    "Hiding the complex implementation details and showing only the essential features of an object."], 
+  "Hiding the complex implementation details and showing only the essential features of an object."
+),
+  new Question(3, 
+    "What does the extends syntax in JavaScript do?",
+     [
+    "Merges two classes into one",
+    "Enables a class to inherit from another",
+    "Creates a copy of another class",
+    "Defines private fields"], 
+  "Enables a class to inherit from another"
+),
+  new Question(4,
+     "How can you append a child element to a parent element in the DOM using JavaScript?", 
+     ["addElement()", "addChild()", "append()", "childAppend()"],
+      "append()"
+    ),
+  new Question(5,
+     "What is the purpose of the addEventListener() method in JavaScript?",
+      [
+    "To select elements by their tag name",
+    "To remove an HTML element",
+    "To attach an event handler to an element",
+    "To change an element's class name"], 
+  "To attach an event handler to an element"
+)];
